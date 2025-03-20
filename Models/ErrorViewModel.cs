2edda8407481c986f59691c6a5a02f07d5d8680c
viniciente <vj181011@gmail.com>
2025-03-20 11:04:19 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PatasLuxos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
