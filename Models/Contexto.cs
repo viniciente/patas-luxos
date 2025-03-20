9115223829ee10984beb935355ddd51bcac3bb7e
viniciente <vj181011@gmail.com>
2025-03-20 14:41:13 -0300

    Classe Pro diogo

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -1,6 +1,13 @@
-﻿namespace PatasLuxos.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PatasLuxos.Models
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente { get; set; }
     }
 }
