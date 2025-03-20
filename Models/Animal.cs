11ab378d6eb52be32c42727c86ccb7512e36bf37
Diogocavalheirojustulin <diogojustulin992@gmail.com>
2025-03-20 15:02:14 -0300

    Classe animal

---
+++ Models/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PatasLuxos.Models
+{
+    public class Animal
+    {
+        [ForeignKey("ClienteId")]
+        [Display(Name = "Cliente")]
+
+        public int ClienteId { get; set; }
+
+        public Cliente? Cliente { get; set; }
+
+        [Column("TipoAnimal")]
+        [Display(Name = "TipoAnimal")]
+        public string TipoAnimal { get; set; } = string.Empty;
+
+
+        [Column("NomeAnimal")]
+        [Display(Name = "NomeAnimal")]
+        public string NomeAnimal { get; set; } = string.Empty;
+
+    }
+}
