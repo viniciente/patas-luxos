47c6ce1f7aa56eda01df553a5a857cb1f06713b5
olucascardoso <lucas.cardoso0990990@gmail.com>
2025-03-20 14:49:56 -0300

    Correção Produtos 1.

--- Models/Produtos.cs
+++ Models/Produtos.cs
@@ -1,6 +1,25 @@
-﻿namespace PatasLuxos.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PatasLuxos.Models
 {
+    [Table("Produtos")]
     public class Produtos
     {
+        [Column("Id")]
+        [Display(Name = "Id do Estabelecimento")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome:")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Column("Descricao")]
+        [Display(Name = "Descrição:")]
+        public string Descricao { get; set; } = string.Empty;
+
+        [Column("Preco")]
+        [Display(Name = "Preço")]
+        public string Preco { get; set; } = string.Empty;
     }
 }
